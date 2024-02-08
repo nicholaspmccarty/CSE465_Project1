dabedf877391aa1de1ad4cd969087d5feaf351e6
mccartnp <nicholasmccarty252@gmail.com>
2024-02-08 14:31:48 -0500

    Added main, and added helper function.

--- src/Zpm.java
+++ src/Zpm.java
@@ -1,5 +1,25 @@
+/*
+ * Nicholas McCarty
+ * CSE 465
+ * Z+- Interpreter
+ */
 public class Zpm {
+
+    /*
+     * Our main method. This method makes sure we recieve an input file
+     * so we can interpret the Z+-.
+     */
     public static void main(String[] args) throws Exception {
-        System.out.println("test");
+        // Requiring a input file
+        if (args.length != 1) {
+            System.out.println("Usage: java Zpm <filename.zpm>");
+            return;
+        }
+        doNext(args[0]);
+    }
+
+    public static void doNext(String fileName) throws Exception {
+        // This is from ChatGPT
+        
     }
 }
